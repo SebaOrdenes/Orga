3e329c985709bd3a7dcf2a544fc949454981abe6
Sebastián Órdenes <sebaordenes1996@gmail.com>
2019-10-04 03:57:26 -0300

    correccion funcion factorial

--- parte2b.asm
+++ parte2b.asm
@@ -1,67 +1,125 @@
 .data
- primermensaje: .asciiz "Entregue el numero a calcular el factorial: "
- segundomensaje: .ascii "\n El factorial es : "
- numero: .word 0
- respuesta: .word 0
 
-
-.text
-
-.globl main 
-main: 
-#leemos el numero ingresado por el usuario
-li $v0, 4
-la $a0, primermensaje
-syscall
- li $v0, 5
- syscall
+ contador : .word 1
+ acumulador: .word 1
+ dos: .word 2
  
- sw $v0, numero
- #llamado a la funcion factorial
- lw $a0, numero
- jal factorial
- sw $v0, respuesta
+ numerofactorial: .word 1 #ESTE PUEDE VARIAR SEGUN EL USUARIO QUIERA 
  
- #mostramos el resultado
-	li $v0, 4
-	la $a0 , segundomensaje
-	syscall
-
- 	 li $v0, 1
-	  lw $a0, respuesta
-	syscall 
-	  li $v0, 10	
-	  syscall
-  
-
-
-.globl factorial
-factorial:
-	subu $sp,$sp,8
-	sw $ra, ($sp)
-	sw $s0, 4($sp)
+.text	
+	.globl main
+	main:
+	lw $t3, contador 
+	lw $t4, numerofactorial
+	lw $t5, acumulador
 	
-	#caso base
+	
+	jal factorial 
+	
+	
+	
+	
+		
+			
+				
+					
+						
+							
+							
+									
+#fUCNION FACTORIAL										
+factorial:		
+	#casos base									
+	beqz $t4, f 
+	beq $t4, 1 ,one 
+	beq $t4, 2, t
+																								
+	loop:
+	#si el contador es IGUAL al factorial estamos ok
+	beq $t3, $t4, exit
+	#sino
+
+	move $t0, $t3
+	move $t1, $t5
+	addi $t3,$t3,1 #Aaumentamos el contador en 1
+	jal multiplicacion #llamamos a nuestra funcion multiplicar 
+	
+	move $t5,$t2 #desplazamos el primer resultado a otro registro y seguimos con el loop
+	j loop
+	
+	
+
+#---------------#
+.globl multiplicacion
+multiplicacion:
+
+
+
+#verifica cual de los dos factores es el mayor, si es el primero se salta a la etiqueta primeromayor
+bgt $t0,$t1, primeromayor
+#de no ser asi trabajaremos con el segundo factor, ya que el que sea menor ira disminuyendo hasta llegar a cero
+#mientras que el mayor se va sumando consigo mismo las veces que sea necesario hasta que el otro factor sea cero.
+ segundomayor:
+ beqz $t0, final
+#RESTAMOS UNO AL PRIMER FACTOR
+ addi $t0, $t0, -1
+ add $t2, $t2, $t1
+ j segundomayor
+ 
+primeromayor:
+beqz $t1,final 
+#restamos uno al segundo factor
+addi $t1, $t1 , -1
+add $t2, $t2, $t0
+j primeromayor
+
+
+#llamado final para cualquiera de las dos opciones anteriores y uego retornamos a la siguiente instruccion, almacenada en $ra
+ final:
+ 	#li $v0, 1
+ 	#move $a0, $t2
+	jr $ra
+	
+	
+	
+exit:
+	#movemos algunos resultados de registros y llamamos nuevamente a la funcion multiplicacion
+	#debido a que el factorial nos llegaba hasta el 3 como minimo numero (error de algoritmo), no el 2, asi que multiplicamos por 2 nuestro ultim oresultado
+	move $t0, $t5
+	lw $t1, dos
+	jal multiplicacion
+	sub $t2, $t2,$t0
+	#llamamos por pantalla
 	li $v0,1
-	beq $a0, 0, hacerfactorial
+	move $a0,$t2
 	
-	#encontrarFactorial 
-	move $s0, $a0
-	sub $a0, $a0, 1
-	jal factorial
+	syscall 
 	
-	#muliplicacion, cambiar poe el algoritmo de la parte 2a
-	mul $v0, $s0 , $v0
+	j fin
+
+one:
+	addi $t2,$t2,1
+	li $v0,1
+	move $a0,$t2
 	
-	hacerfactorial:
-		lw $ra, ($sp)
-		lw $s0, 4($sp)
-		addu $sp, $sp,8
-		jr $ra
-		
-		
+	syscall
+	j fin
+	
+f:
+	addi $t2,$zero,0
+	li $v0,1
+	move $a0,$t2
+	
+	syscall
+	j fin
+
+t:
+	addi $t2,$zero,2
+	li $v0,1
+	move $a0,$t2
+	syscall
 	
 	
 	
-	
+fin:
 	
